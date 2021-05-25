2d245c63e783b746e2f23ef1cac5948268b5095c
undersky <don0910129285@gmail.com>
2021-05-25 22:58:29 +0800

    axios get game data from laravel api

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mx-auto px-2 md:px-8">
+    <GameList title="Game List"/>
+    <div class="grid md:grid-cols-3">
+      <GameCard v-bind:key="game" v-bind:game="game" v-for="game in games"/>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GameList from './components/GameList.vue'
+import GameCard from './components/GameCard.vue'
+import axios from "axios"
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GameList,
+    GameCard
+  },
+  data() {
+    return {
+      games: ''
+    }
+  },
+  mounted() {
+    axios
+    .get('http://127.0.0.1/eshop-price/index.php/api/v1/game')
+    .then((response) => { this.games = response.data; })
+    .catch(function (error) { // 请求失败处理
+      console.log(error);
+    });
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  @apply mt-5;
 }
 </style>
---
+++ src/components/GameCard.vue
@@ -0,0 +1,73 @@
+<template>
+  <a class="flex-grow hover:bg-gray-100" href="#">
+    <div class="flex flex-row game-card">
+      <div class="game-boxart">
+        <img  v-bind:src="game.Boxart"/>
+      </div>
+      <div class="game-data">
+        <div class="game-title">{{ game.Title }}</div>
+        <div>
+          <span class="all-chinese">全區中文</span>
+          <span class="lowest-price">史低</span>
+        </div>
+        <div class="price-group">
+          <span class="sale-price">{{ game.Price }}</span> 
+          <s class="msrp">{{ game.MSRP }}</s>
+          <span class="country">(美國)</span>
+        </div>
+        <div class="discount-box">{{ game.Discount }}% 折扣</div>
+      </div>
+    </div>
+  </a>
+</template>
+
+<script>
+export default {
+  name: 'GameList',
+  props: {
+    game: Object
+  },
+  mounted() {
+    console.log(this.game);
+  }
+}
+</script>
+
+<style scoped>
+.game-card {
+  @apply my-1 divide-y divide-gray-400;
+}
+.game-boxart {
+  @applt w-3/12 md:w-5/12;
+}
+.game-boxart > img {
+  @apply float-right h-20 w-20 md:h-36 md:w-36;
+}
+.game-data {
+  @apply flex flex-col pl-2 h-20 w-9/12 md:h-36 md:w-7/12 relative;
+}
+.game-title {
+  @apply md:text-2xl font-bold;
+}
+.all-chinese {
+  @apply bg-gray-200 text-red-600 px-1 mr-1 rounded;
+}
+.lowest-price {
+  @apply bg-gray-200 text-blue-600 px-1 mr-1 rounded;
+}
+.price-group {
+  @apply absolute bottom-0 left-2;
+}
+.sale-price {
+  @apply text-base mr-1 text-red-600;
+}
+.msrp {
+  @apply text-xs mr-1 text-gray-400;
+}
+.country {
+  @apply text-sm text-gray-400;
+}
+.discount-box {
+  @apply bg-red-500 text-white pl-2 pr-1 md:pl-3 md:pr-2 rounded-tl-xl absolute bottom-0 right-0 md:right-2;
+}
+</style>
