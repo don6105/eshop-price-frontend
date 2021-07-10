f7dda722f369c8b14bb1be44af3f3a01c1f23e0c
undersky <don0910129285@gmail.com>
2021-07-10 15:51:47 +0800

    add view SummaryGroup.

--- src/components/SummaryCard.vue
+++ src/components/SummaryCard.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="summary-card" ref="el_card">
     <div v-if="direction === 'left'" class="summary-action">
-      <button>
-        <svg width="100%" height="100">
-          <line x1="36" y1="7"  x2="11" y2="50" style="stroke:white; stroke-width:8"/>
-          <line x1="36" y1="93" x2="11" y2="50" style="stroke:white; stroke-width:8"/>
-          <circle cx="36" cy="7"  r="4" fill="white"/>
+      <button @click="removeFromSummarys">
+        <svg width="50" height="100" viewBox="0 0 50 135" preserveAspectRatio="xMidYMid meet">
+          <line x1="36" y1="13" x2="11" y2="50" style="stroke:white; stroke-width:8"/>
+          <line x1="36" y1="87" x2="11" y2="50" style="stroke:white; stroke-width:8"/>
+          <circle cx="36" cy="13" r="4" fill="white"/>
           <circle cx="11" cy="50" r="4" fill="white"/>
-          <circle cx="36" cy="93" r="4" fill="white"/>
+          <circle cx="36" cy="87" r="4" fill="white"/>
         </svg>
       </button>
     </div>
@@ -16,18 +16,23 @@
     </div>
     <div class="summary-box">
       <div class="game-title">{{ summary.Title }} </div>
-      <div class="game-country" :class="country_color[summary.Country]">
-        {{ summary.Country }}
+      <div class="mt-1">
+        <div class="game-country" :class="country_color[summary.Country]">
+          {{ summary.Country }}
+        </div>
+        <div class="game-group" :class="{ active: (summary.GroupID > 0) }">
+          {{ summary.GroupID }}
+        </div>
       </div>
     </div>
     <div v-if="direction === 'right'" class="summary-action">
-      <button>
-        <svg width="100%" height="100">
-          <line x1="12" y1="7"  x2="39" y2="50"  style="stroke:white;stroke-width:8"/>
-          <line x1="12" y1="93" x2="39" y2="50"  style="stroke:white;stroke-width:8"/>
-          <circle cx="12" cy="7"  r="4" fill="white"/>
+      <button @click="removeFromGroups">
+        <svg width="50" height="100" viewBox="0 0 50 135" preserveAspectRatio="xMidYMid meet">
+          <line x1="12" y1="13" x2="39" y2="50"  style="stroke:white;stroke-width:8"/>
+          <line x1="12" y1="87" x2="39" y2="50"  style="stroke:white;stroke-width:8"/>
+          <circle cx="12" cy="13" r="4" fill="white"/>
           <circle cx="39" cy="50" r="4" fill="white"/>
-          <circle cx="12" cy="93" r="4" fill="white"/>
+          <circle cx="12" cy="87" r="4" fill="white"/>
         </svg>
       </button>
     </div>
@@ -53,7 +58,20 @@ export default {
     };
   },
   methods: {
-
+    removeFromSummarys(event) {
+      this.$refs.el_card.classList.add('left-out');
+      setTimeout(() => {
+        this.$refs.el_card.classList.remove('left-out');
+        this.$parent.removeFromSummarys(this.index);
+      }, 200);
+    },
+    removeFromGroups() {
+      this.$refs.el_card.classList.add('right-out');
+      setTimeout(() => {
+        this.$refs.el_card.classList.remove('right-out');
+        this.$parent.removeFromGroups(this.index);
+      }, 200);
+    }
   }
 }
 </script>
--- src/views/GameGroup.vue
+++ src/views/GameGroup.vue
@@ -3,16 +3,29 @@
   
   <div class="row-box">
     <div class="h-full">
-      <div class="h-1/2 pb-2">
+      <div class="h-3/5 pb-2">
         <div class="data-box h-full border-blue-300">
           <input type="text" class="search-input">
-
+          <button class="search-btn">Search</button>
+          <div class="float-right mt-0.5">
+            <input type="text" class="group-input" placeholder="GroupID">
+            <button class="group-btn">Apply</button>
+          </div>
+          <div class="card-container">
+            <SummaryCard 
+              v-for="(summary, index) in groups"
+              :key="summary"
+              :index="index"
+              direction='right'
+              :summary="summary"
+            />
+          </div>
         </div>
       </div>
-      <div class="h-1/2 pt-2">
+      <div class="h-2/5 pt-2">
         <div class="border-yellow-300 data-box h-full ">
           <input type="text" class="search-input">
-
+          <button class="search-btn">Search</button>
         </div>
       </div>
     </div>
@@ -20,10 +33,12 @@
   <div class="row-box">
     <div class="data-box h-full border-blue-300">
       <input type="text" class="search-input">
-      <div class="w-full overflow-y-auto">
+      <button class="search-btn">Search</button>
+      <div class="card-container">
         <SummaryCard 
-          v-for="summary in summarys"
+          v-for="(summary, index) in summarys"
           :key="summary"
+          :index="index"
           direction='left'
           :summary="summary"
         />
@@ -49,25 +64,29 @@ export default {
           ID: 7,
           Boxart: 'https://store.nintendo.com.hk/media/catalog/product/cache/559013652f8c31ac036888adc145c37f/1/1/110.jpg',
           Title: '薩爾達傳說 曠野之息',
-          Country: 'hk'
+          Country: 'hk',
+          GroupID: 1
         },
         {
           ID: 558,
           Boxart: 'https://assets.nintendo.com/image/upload/ncom/en_US/games/switch/t/the-legend-of-zelda-breath-of-the-wild-switch/hero',
           Title: 'The Legend of Zelda™: Breath of the Wild',
-          Country: 'us'
+          Country: 'us',
+          GroupID: 1
         },
         {
           ID: 256,
           Boxart: 'https://store.nintendo.com.hk/media/catalog/product/cache/559013652f8c31ac036888adc145c37f/b/e/be754ef587d726f5337f294382e7411183e3785edd723d8c17fa7149d4582dde.jpg',
           Title: '泰拉瑞亞',
-          Country: 'hk'
+          Country: 'hk',
+          GroupID: 2
         },
         {
           ID: 3378,
           Boxart: 'https://assets.nintendo.com/image/upload/ncom/en_US/games/switch/t/terraria-switch/hero',
           Title: 'Terraria',
-          Country: 'us'
+          Country: 'us',
+          GroupID: 0
         },
       ],
       wiki_data: []
@@ -91,6 +110,14 @@ export default {
         .catch(function (error) {
           console.log(error);
         });
+    },
+    removeFromSummarys(index) {
+      this.groups.unshift(this.summarys[index]);
+      this.summarys.splice(index, 1);
+    },
+    removeFromGroups(index) {
+      this.summarys.unshift(this.groups[index]);
+      this.groups.splice(index, 1);
     }
   },
   mounted() {
